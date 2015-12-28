c6ee5edbeee107336649fc0aa72ea117a7587d1c
Martin Bureš <m2bures@gmail.com>
2015-12-28 18:02:56 +0100

    added collection read demo;

--- app/controllers/SchemesController.php
+++ app/controllers/SchemesController.php
@@ -4,42 +4,50 @@ namespace CpdnAPI\Controllers;
 
 use Phalcon\Http\Response;
 use CpdnAPI\Models\Network\Scheme;
+use CpdnAPI\Utils\Common;
 
 class SchemesController extends ControllerBase {
 	public function initialize() {
 		$this->view->disable ();
 	}
-	
-	public function readCollectionAction(){
+	public function readCollectionAction() {
+		switch ($this->dispatcher->getParam ( "version" )) {
+			case Common::API_VERSION_V1 :
+				$response = new Response ();
+				$response->setStatusCode ( 200, "OK" );
+				$schemes = Scheme::find ();
+				$r = array ();
+				
+				foreach ( $schemes as $s ) {
+					$r [] = array (
+							$s->id,
+							$s->name 
+					);
+				}
+				$response->setJsonContent ( $r );
+				
+				return $response;
+				break;
+		}
 	}
-	
-	public function createItemAction(){
+	public function createItemAction() {
 	}
-	
-	public function readItemAction(){
+	public function readItemAction() {
 	}
-	
-	public function updateItemAction(){
+	public function updateItemAction() {
 	}
-	
-	public function deleteItemAction(){
+	public function deleteItemAction() {
 	}
-	
-	public function readNodesCollectionAction(){
+	public function readNodesCollectionAction() {
 	}
-	
-	public function readSectionsCollectionAction(){
+	public function readSectionsCollectionAction() {
 	}
-	
-	public function readMapPointsCollectionAction(){
+	public function readMapPointsCollectionAction() {
 	}
-	
-	public function readObjectsCollectionAction(){
+	public function readObjectsCollectionAction() {
 	}
-	
-	public function readPermissionsCollectionAction(){
+	public function readPermissionsCollectionAction() {
 	}
-	
-	public function readTasksCollectionAction(){
+	public function readTasksCollectionAction() {
 	}
 }
