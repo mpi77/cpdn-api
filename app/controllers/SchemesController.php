c9a100db85df3cf63cdba51c0f7fb4e2e6be6464
Martin Bureš <m2bures@gmail.com>
2016-01-06 21:04:41 +0100

    removed *Criteria* in SchemesCtrl.readCollection(); implemented *QueryBuilder* and *Paginator* SchemesCtrl.readCollection();

--- app/controllers/SchemesController.php
+++ app/controllers/SchemesController.php
@@ -5,9 +5,11 @@ namespace CpdnAPI\Controllers;
 use CpdnAPI\Models\Network\Scheme;
 use CpdnAPI\Utils\Common;
 use CpdnAPI\Utils\CollectionGenerator as CG;
+use CpdnAPI\Utils\MetaGenerator as MG;
 use CpdnAPI\Utils\Searchable;
 use CpdnAPI\Utils\Sortable;
-use Phalcon\Mvc\Model\Criteria;
+use CpdnAPI\Utils\Paginator as PAG;
+use Phalcon\Paginator\Adapter\QueryBuilder as PQB;
 
 class SchemesController extends ControllerBase {
 	private $validFields = array (
@@ -25,27 +27,41 @@ class SchemesController extends ControllerBase {
 		$this->response->setStatusCode ( 404, "Not Found" );
 	}
 	public function readCollectionAction() {
+		$page_size = (int) $this->request->get ( "pageSize", "int", PAG::DEFAULT_PAGE_SIZE );
+		$page_number = (int) $this->request->get ( "pageNumber", "int", PAG::DEFAULT_PAGE );
+		
 		switch ($this->dispatcher->getParam ( "version" )) {
 			case Common::API_VERSION_V1 :
 				$this->response->setStatusCode ( 200, "OK" );
 				
-				$query = Criteria::fromInput ( $this->di, 'CpdnAPI\Models\Network\Scheme', Searchable::buildCriteriaFromInputParams($this->request->get ( "q" ),$this->validFields) );
+				// create builder
+				$builder = $this->modelsManager->createBuilder ()->columns ( array_keys ( $this->validFields ) )->from ( 'CpdnAPI\Models\Network\Scheme' );
 				
-				if ($s = Sortable::buildCriteriaOrderByParams($this->request->get ( "s" ),$this->validFields)) {
-					$query->orderBy ( $s );
+				// append WHERE conditions
+				if (($where = Searchable::buildQueryBuilderWhereParams ( $this->request->get ( "q" ), $this->validFields )) !== false) {
+					$builder->where ( $where ["conditions"], $where ["bindParams"] );
 				}
 				
-				$schemes = Scheme::find ( $query->getParams () );
-				$r = array ();
-				
-				foreach ( $schemes as $s ) {
-					$r [] = array (
-							$s->id,
-							$s->name 
-					);
+				// append ORDER BY string
+				if (($order = Sortable::buildQueryBuilderOrderByParams ( $this->request->get ( "s" ), $this->validFields )) !== false) {
+					$builder->orderBy ( $order );
 				}
-				$this->response->setJsonContent ( CG::generate($r) );
 				
+				// paginate builder
+				$paginator = new PQB ( array (
+						"builder" => $builder,
+						"limit" => $page_size,
+						"page" => $page_number 
+				) );
+				$page = $paginator->getPaginate ();
+				
+				// expand & select result set
+				$items = array ();
+				foreach ( $page->items as $item ) {
+					$items [] = MG::generate(sprintf("/schemes/%s", $item->id), array(MG::KEY_ID => $item->id));
+				}
+				
+				$this->response->setJsonContent ( CG::generate ( $items, $this->request->getURI (), $page->total_items, $page->total_pages, $page->current, $page_size ) );
 				return $this->response;
 				break;
 		}
