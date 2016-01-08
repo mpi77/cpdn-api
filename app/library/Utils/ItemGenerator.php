8bfe4b2136326fd151e24873956e11c514c5700e
Martin Bureš <m2bures@gmail.com>
2016-01-08 23:04:52 +0100

    added + impl ItemGenerator;

--- app/library/Utils/ItemGenerator.php
+++ app/library/Utils/ItemGenerator.php
@@ -2,5 +2,12 @@
 
 namespace CpdnAPI\Utils;
 
+use CpdnAPI\Utils\MetaGenerator as MG;
+
 class ItemGenerator {
+	public static function generate($queryUri, $metaArgs = array(), $bodyArgs = array()) {
+		$r = array();
+		$r[MG::KEY_META] = MG::generate ( $queryUri, $metaArgs);
+		return array_merge ( $r, $bodyArgs );
+	}
 }
