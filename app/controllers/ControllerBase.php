21b7b5985a92796fd01594ee43486e96e3d27903
Martin Bureš <m2bures@gmail.com>
2016-01-27 20:28:25 +0100

    implemented Auth in CtrlBase

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -7,6 +7,16 @@ use Phalcon\Mvc\Dispatcher;
 
 class ControllerBase extends Controller {
 	public function beforeExecuteRoute(Dispatcher $dispatcher) {
-		// check OAuth and ACL
+		
+		$r = $this->auth->isAuthorized($this->request);
+		$controller = $dispatcher->getControllerName ();
+		
+		if ($r !== true && $controller != "errors") {
+			$dispatcher->forward ( array (
+					'controller' => 'errors',
+					'action' => 'e403' 
+			) );
+			return false;
+		}
 	}
 }
